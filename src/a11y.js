bc825fb0d56efc5c1afb5673ba50b6c1c10a8222
Adrian D. Alvarez <adrian@adriandalvarez.com>
2019-08-21 16:29:30 -0400

    Transpile a11y.js to es2015 for ie11 support

--- src/a11y.js
+++ src/a11y.js
@@ -1,13 +1,22 @@
-import * as head from './head';
-import * as body from './body';
+"use strict";
 
-const a11yChecker = () => {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var head = _interopRequireWildcard(require("./head"));
+
+var body = _interopRequireWildcard(require("./body"));
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
+var a11yChecker = function a11yChecker() {
   head.hasDocumentType();
   head.hasDocumentTitle();
   head.hasDocumentLanguage();
   head.hasDocumentMetaCharset();
   head.hasDocumentScalable();
-
   body.hasHeadingOnce();
   body.hasImagesAlt();
   body.hasLinksText();
@@ -24,4 +33,5 @@ const a11yChecker = () => {
   body.hasDuplicateIds();
 };
 
-export default a11yChecker;
+var _default = a11yChecker;
+exports.default = _default;
